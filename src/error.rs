55b826cdd9825c9e47c258425c36547a8c8cbc66
zhangguokai <zhangguokai@inspur.com>
2024-12-12 23:37:46 +0800

    feat: 修正返回值与异常封装器

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
+use std::sync::PoisonError;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BmbpAuthErr {
@@ -14,6 +15,12 @@ impl BmbpAuthErr {
             message,
         }
     }
+    pub fn lock_err(message: String) -> Self {
+        BmbpAuthErr {
+            kind: BmbpAuthErrKind::LockError,
+            message,
+        }
+    }
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -24,25 +31,33 @@ pub enum BmbpAuthErrKind {
     UserNotFound,
     PasswordNotValid,
     NoImpl,
+    LockError,
 }
 
 impl Display for BmbpAuthErr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let str = match self.kind {
-            BmbpAuthErrKind::NotLogin => format!("[{}]{}:{}", "0000", "未登录", self.message),
-            BmbpAuthErrKind::TokenValid => format!("[{}]{}:{}", "0004", "token无效", self.message),
+            BmbpAuthErrKind::NotLogin => format!("[{}]{}:{}", "1001", "未登录", self.message),
+            BmbpAuthErrKind::TokenValid => format!("[{}]{}:{}", "1004", "token无效", self.message),
             BmbpAuthErrKind::TokenExpire => {
-                format!("[{}]{}:{}", "0005", "token已过期", self.message)
+                format!("[{}]{}:{}", "1005", "token已过期", self.message)
             }
             BmbpAuthErrKind::UserNotFound => {
-                format!("[{}]{}:{}", "0002", "用户不存在", self.message)
+                format!("[{}]{}:{}", "1002", "用户不存在", self.message)
             }
             BmbpAuthErrKind::PasswordNotValid => {
-                format!("[{}]{}:{}", "0003", "用户名或密码不正确", self.message)
+                format!("[{}]{}:{}", "1003", "用户名或密码不正确", self.message)
             }
-            BmbpAuthErrKind::NoImpl => format!("[{}]{}:{}", "0000", "缺少实现", self.message),
+            BmbpAuthErrKind::NoImpl => format!("[{}]{}:{}", "1000", "缺少实现", self.message),
+            BmbpAuthErrKind::LockError => format!("[{}]{}:{}", "0000", "获取锁失败", self.message),
         };
         write!(f, "{}", str)
+    }
+}
+
+impl<T> From<PoisonError<T>> for BmbpAuthErr {
+    fn from(err: PoisonError<T>) -> Self {
+        BmbpAuthErr::lock_err(err.to_string())
     }
 }
 
